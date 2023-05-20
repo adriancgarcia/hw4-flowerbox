40ad5b32a6a0c0f5d924249ac8e53a6a7c15a4dc
adriancgarcia <adriancgarcia@gmail.com>
2023-05-19 22:36:39 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -7,20 +7,31 @@
 // console.dir(document.body)
 const $body = $("body")
 // console.log($body)
- 
 
-$(() => {
+// $(() => {
     
-});
+// });
 
-const $container = $("container")
-    // console.log($container)
-
+const $container = $("#container")
+    console.log($container);
 
 const $h1 = $("<h1>")
 $h1.text("Hogwarts")
-$("div").append($h1)
-// console.log($h1)
+console.log($h1)
+$container.append($h1)
 
+const $h2 = $("<h2>")
+$h2.text("Adrian")
+$container.append($h2)
 
-    
+const $h3 = $("<h3>")
+$h3.text("Slytherin")
+$container.append($h3)
+
+const $h4pet = $("<h4>")
+$h4pet.addClass("dog").text("Khaleesi")
+$container.append($h4pet)    
+
+const $h4wand = $("<h4>")
+$h4wand.addClass("wand").text("Holly Wand with Unicorn Hair")
+$container.append($h4wand)
