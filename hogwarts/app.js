85174259f2cdc60db3c24051be6a5205df2844a2
adriancgarcia <adriancgarcia@gmail.com>
2023-05-19 20:47:21 -0500

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,3 +1,26 @@
 // if (typeof jQuery =='undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 // } else {console.log('I did it! I linked jQuery and this js file!')};
+
+
+// document.querySelector("body")
+// console.dir(document.body)
+const $body = $("body")
+// console.log($body)
+ 
+
+$(() => {
+    
+});
+
+const $container = $("container")
+    // console.log($container)
+
+
+const $h1 = $("<h1>")
+$h1.text("Hogwarts")
+$("div").append($h1)
+// console.log($h1)
+
+
+    
