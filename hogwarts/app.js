fa03dc02cc23ca2d32239f264f420b661c2a4ec7
adriancgarcia <adriancgarcia@gmail.com>
2023-05-22 22:49:25 -0500

    Commit 4 - I realize I can make things that are ever increasingly more complex with greater ease. Adding order is always more effort than causing chaos

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -49,6 +49,49 @@ $("ul").append($("<li>").addClass("dog").text("leash"))
 
 $("ul").append($("<li>").text("Bertie Boys Every Flaor[Jelly] Beans"))
 
+const $div2 = $("<div2>")
+$div2.attr("id", "table")
+$container.append($div2)
+
+const $table = $("<table>"); 
+// $table.after($("h5"))
+// console.log($table)
+$div2.append($table)
+
+const $h5 = $("<h5>");-
+$h5.text("Spring 2017")
+// $("h5").before($(div));
+$div2.prepend($h5);
+// $h5.before($table);
+
+const $thead = $("<thead>");
+// $(thead).after($("h5"))
+$table.append($thead)
+
+$("thead").append($("<th>").text("Day"))
+$("thead").append($("<th>").text("Classes"))
+
+
+$("table").append($("<tr>"));
+$("tr").append($("<td>").text("Monday"));
+$("tr").append($("<td>").text("Herbology", "Divination", "History of Magic"));
+
+$("table").append($("<tr>"));
+$("tr").append($("<td>").text("Tuesday"))
+$("tr").append($("<td>").text("Charms", "potions"));
+
+$("table").append($("<tr>"));
+$("tr").append($("<td>").text("Wednesday"))
+$("tr").append($("<td>").text("Transfiguration", "Defense Against the Dark Arts"));
+
+$("table").append($("<tr>"));
+$("tr").append($("<td>").text("Thursday"))
+$("tr").append($("<td>").text("Charms"));
+
+$("table").append($("<tr>"));
+$("tr").append($("<td>").text("Friday"))
+$("tr").append($("<td>").text("Defense Against the Dark Arts"));
+
 });
 
 
